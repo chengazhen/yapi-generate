3cfe65ec57c44431096aac5db6f1ec77749d5b76
chengchongzhen <chengchongzhen@hexfuture.net>
2023-10-25 10:13:57 +0800

    feat: 拉取json数据时捕获错误

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var http  = require('../lib/http')
+var http = require('../lib/http')
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-  http.get(req.body.url).then(_res => {
-     res.send(_res.data)
-  })
+router.post('/', async function (req, res, next) {
+  try {
+    const _res = await http.get(req.body.url)
+    res.send(_res.data)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 });
 
 module.exports = router;
